e362e5978fab0a633ecb77f2a73c114ce3d26395
Vivek Parmar <Vivek.Parmar@einfochips.com>
2024-05-20 19:56:46 +0530

    Moved favicon images and related files

--- components/meta/MetaTags.tsx
+++ components/meta/MetaTags.tsx
@@ -6,21 +6,21 @@ export default function MetaTags() {
         <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/meta/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/meta/favicon-32x32.png"
+            href="/favicon-32x32.png"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/meta/favicon-16x16.png"
+            href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/meta/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#162447" />
         <meta name="robots" content="index, follow" />
