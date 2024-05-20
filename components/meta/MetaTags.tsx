d7468ed12c913d28266d1c46697a15bf1bd8069d
Vivek Parmar <Vivek.Parmar@einfochips.com>
2024-05-20 19:29:36 +0530

    Added meta Tags

---
+++ components/meta/MetaTags.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function MetaTags() {
+  return (
+    <>
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/meta/apple-touch-icon.png"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/meta/favicon-32x32.png"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/meta/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/meta/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#162447" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+
+        <title>Vivek Parmar Portfolio</title>
+        <meta name="title" content="Vivek Parmar" />
+        <meta name="application-name" content="Vivek Parmar Portfolio" />
+        <meta name="apple-mobile-web-app-title" content="Vivek Parmar Portfolio" />
+        <meta property="og:title" content="Vivek Parmar Portfolio" />
+        <meta name="twitter:title" content="Vivek Parmar Portfolio" />
+        <meta property="og:description" content="Vivek's Portfolio Website with his past experiences, projects, resume." />
+        <meta name="twitter:description" content="Vivek's Portfolio Website with his past experiences, projects, resume." />
+        <meta name="author" content="Vivek Parmar"></meta>
+        <meta name="description" content= "Vivek Parmar Portfolio" />
+        <meta name="keywords" content="Vivekparmar999, Vivekparmar999 portfolio, vivek parmar protfolio, vivek parmar resume,vivek parmar computer science, vivek parmar, vivek parmar einfochips, vivek parmar Charusat university, vivek parmar basic portfolio"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </>
+  );
+}
